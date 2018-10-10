294d45914d08e8e3ebbb317bf85569fb7822d3f2
amshelhack3r <amshelhack3r@gmail.com>
2018-10-10 12:37:17 +0300

    switch to local mode

--- config/config.js
+++ config/config.js
@@ -1,6 +1,14 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "mutallde_root",
-  PASSWORD: ")]9(p'Mj@ccp",
-  DBNAME: "mutall_eureka"
+  remote: {
+    HOST: "localhost",
+    USER: "mutallde_root",
+    PASSWORD: ")]9(p'Mj@ccp",
+    DBNAME: "mutallde_eureka_api"
+  },
+  local: {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "",
+    DBNAME: "mutall_eureka"
+  }
 };
--- database.js
+++ database.js
@@ -1,22 +1,22 @@
 //bring in mysql
 const mysql = require("mysql");
 //get the config file
-const config = require("./config/config");
+const config = require("./config/config").local;
 
 //create a mysql connection
 const conn = mysql.createConnection({
-    host: config.HOST,
-    user: config.USER,
-    password: config.PASSWORD,
-    database: config.DBNAME
+  host: config.HOST,
+  user: config.USER,
+  password: config.PASSWORD,
+  database: config.DBNAME
 });
 
 conn.connect(err => {
-    if (err) {
-        console.log(`Connection error ${err.stack}`);
-        return;
-    }
-    console.log(`Connected as id ${conn.threadId}`);
+  if (err) {
+    console.log(`Connection error ${err.stack}`);
+    return;
+  }
+  console.log(`Connected as id ${conn.threadId}`);
 });
 
 module.exports = conn;
