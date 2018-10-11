26bd7b0ec75300c11926f11c31c239dd6bd5aee0
amshelhack3r <amshelhack3r@gmail.com>
2018-10-11 16:09:55 +0300

    handlebars middleware

--- server.js
+++ server.js
@@ -2,8 +2,16 @@
 const express = require("express");
 //get body parser
 const bodyParser = require("body-parser");
+//template engine
+const exphbs = require("express-handlebars");
+//import path
+const path = require("path");
 
 const app = express();
+
+//handle middleware
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // parse application/x-www-form-urlencoded
 app.use(
@@ -12,14 +20,15 @@ app.use(
   })
 );
 
+//set the path to the public directory
+app.use(express.static(path.join(__dirname, "public")));
+
 // parse application/json
 app.use(bodyParser.json());
 
-
-//require the routes we have created 
-const routes = require('./routes/crud');
-app.use('/', routes);
-
+//require the routes we have created
+const routes = require("./routes/crud");
+app.use("/", routes);
 
 const port = 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
