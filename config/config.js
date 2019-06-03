3d4ff83be95b69193c7d00d2560b953ffff2136f
amshel <amshelhack3r@gmail.com>
2019-06-03 12:54:27 +0000

    initial commit

--- config/config.js
+++ config/config.js
@@ -2,14 +2,14 @@ module.exports = {
   remote: {
     HOST: "localhost",
     USER: "mutallde_root",
-    PASSWORD: ")]9(p'Mj@ccp",
+    PASSWORD: "mutalldatamanagers",
     DBNAME: "mutallde_eureka_api"
   },
   local: {
     HOST: "localhost",
     USER: "root",
     PASSWORD: "",
-    DBNAME: "mutall_eureka"
+    DBNAME: "eureka"
   },
   rootpath: "/node_projects/api"
 };
